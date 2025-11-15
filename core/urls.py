c27f374ceba0d577afdf1c95e2597231b4962cf8
HenriqueANunes <henrique.a.nunes@outlook.com>
2025-11-15 20:49:54 -0300

    Corrigido url dos endpoint para terminarem com '/'.

--- core/urls.py
+++ core/urls.py
@@ -7,12 +7,12 @@ urlpatterns = [
     path("login/cadastro/", core.views.UserCadastroView.as_view(), name="user_cadastro"),
     path("logout/", LogoutView.as_view(next_page="login"), name="logout"),
 
-    path("home", core.views.ChamadoHomeView.as_view(), name="home"),
-    path("home/chamado", core.views.ChamadoCadastroView.as_view(), name="home_chamado"),
-    path("home/chamado/<int:chamado_id>", core.views.ChamadoCadastroView.as_view(), name="home_chamado"),
+    path("home/", core.views.ChamadoHomeView.as_view(), name="home"),
+    path("home/chamado/", core.views.ChamadoCadastroView.as_view(), name="home_chamado"),
+    path("home/chamado/<int:chamado_id>/", core.views.ChamadoCadastroView.as_view(), name="home_chamado"),
 
-    path('vue/login', core.views.VueLoginView.as_view(), name='vue_login'),
-    path('vue/cadastro', core.views.VueCadastroView.as_view(), name='vue_cadatro'),
-    path('vue/home', core.views.VueHomeView.as_view(), name='vue_app'),
+    path('vue/login/', core.views.VueLoginView.as_view(), name='vue_login'),
+    path('vue/cadastro/', core.views.VueCadastroView.as_view(), name='vue_cadatro'),
+    path('vue/home/', core.views.VueHomeView.as_view(), name='vue_app'),
 
 ]
